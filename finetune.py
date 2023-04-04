beb005e3ee6ac456e44d8af07b323dc619af2840
费政聪 <3300949577@qq.com>
2023-04-04 15:25:57 +0800

    Add files via upload

---
+++ finetune.py
@@ -0,0 +1,83 @@
+import os 
+import torch.nn as nn 
+import transformers
+
+from llama import LlamaTokenizer, LlamaForCausalLM
+from peft import (
+    prepare_model_for_int8_training,
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+)
+from utils import TextDataSet
+
+
+
+ckpt_path = './ckpt'
+tokenizer = LlamaTokenizer.from_pretrained(ckpt_path)
+model = LlamaForCausalLM.from_pretrained(ckpt_path) 
+MICRO_BATCH_SIZE = 4  # this could actually be 5 but i like powers of 2
+BATCH_SIZE = 128
+GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
+EPOCHS = 3  # we don't always need 3 tbh
+LEARNING_RATE = 3e-4  # the Karpathy constant
+CUTOFF_LEN = 256  # 256 accounts for about 96% of the data
+LORA_R = 8
+LORA_ALPHA = 16
+LORA_DROPOUT = 0.05
+VAL_SET_SIZE = 2000
+TARGET_MODULES = [
+    "q_proj",
+    "v_proj",
+]
+DATA_PATH = "./data/alpaca_chinese.json"
+model = prepare_model_for_int8_training(model)
+
+config = LoraConfig(
+    r=LORA_R,
+    lora_alpha=LORA_ALPHA,
+    target_modules=TARGET_MODULES,
+    lora_dropout=LORA_DROPOUT,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+model = get_peft_model(model, config)
+tokenizer.pad_token_id = 0 
+
+train_data = TextDataSet(DATA_PATH, tokenizer=tokenizer)
+eval_data = TextDataSet(DATA_PATH, tokenizer=tokenizer)
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=eval_data,
+    args=transformers.TrainingArguments(
+        per_device_train_batch_size=MICRO_BATCH_SIZE,
+        gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
+        warmup_steps=100,
+        num_train_epochs=EPOCHS,
+        learning_rate=LEARNING_RATE,
+        fp16=True,
+        logging_steps=20,
+        evaluation_strategy="steps",
+        save_strategy="steps",
+        eval_steps=200,
+        save_steps=200,
+        output_dir="lora-alpaca",
+        save_total_limit=3,
+        load_best_model_at_end=True,
+        ddp_find_unused_parameters=None,
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+model.config.use_cache = False
+
+old_state_dict = model.state_dict
+model.state_dict = (
+    lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
+).__get__(model, type(model))
+
+trainer.train()
+
+model.save_pretrained("lora")
+
